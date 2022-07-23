6b66f8814145120882312f2cffe07cfa7d76f3d9
Yorch Sepúlveda <yorch5.77@gmail.com>
2022-07-23 13:33:26 -0400

    "Actividad terminada"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, length: { minimum: 2, too_short:  ": El nombre debe contener minimo 2 caracteres" } 
+    validates :last_name, presence: true, length: { minimum: 2, too_short: ": El apellido debe contener minimo 2 caracteres" }
+    validates :email_address, presence: true#, :message => "Debe ingresar un corrreo"
+    validates :age, presence: true, numericality: { greater_than: 10, less_than: 150, message: ": La edad minima a ingresar es de 10 años y la maxima es de 150 años" }#, :message => "La edad minima a ingresar es de 10 años y la maxima es de 150 años"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2022_07_23_153227) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "email_address"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
